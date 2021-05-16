9f01dfd8306de256a3a3d70061c3b29512a8eba5
Rem0ld <p.lovergne@hotmail.fr>
2021-05-16 21:54:41 +0200

    modif header with logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import React, { ReactElement } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../public/logo_with_plane.svg";
 import MenuDropdown from "./elements/MenuDropdown";
 
 export default function Header(): ReactElement {
   return (
-    <div className="h-16 pl-4 pr-8 flex justify-between items-center shadow-md bg-white">
-      <span>logo</span>
+    <div className="h-16 pl-4 pr-8 flex justify-between items-center shadow-lg bg-white">
+      <Link to="/">
+        <img src={logo} alt="logo" width="80px" height="80px" />
+      </Link>
       <MenuDropdown notifCount={0} pseudo="PierrotLeFou" isConnected />
     </div>
   );
