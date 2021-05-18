c16f16b09db2590fda461fe0c072ac5e44546a68
Rem0ld <p.lovergne@hotmail.fr>
2021-05-18 18:59:09 +0200

    made a list to help for banned country

---
+++ src/api/listDangerousCountry.ts
@@ -0,0 +1,5 @@
+const list = ["Afghanistan", "Bahrain", "Burkina Faso", "Burundi", "Comores", "Djibouti", "Ethiopia", "Irak", "Iran", "Jordania", "Koweit", "Mali", "Mauritania", "Niger", "Ouzbek", "Pakistan", "Somalia",
+  "Sudan", "Syria", "Tchad", "Yemen"]
+
+export default list
+
