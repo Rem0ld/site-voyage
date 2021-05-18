bc8227f184aef3967a61479650d7fd2f9ec24c25
Rem0ld <p.lovergne@hotmail.fr>
2021-05-18 19:00:56 +0200

    fn promise getting all countries

--- src/api/GetCountries.ts
+++ src/api/GetCountries.ts
@@ -2,6 +2,10 @@ import { Country } from "../types";
 
 const url = "https://restcountries.eu/rest/v2/all"
 
-export default async function getFruits(): Promise<Country[]> {
-  return (await fetch(url)).json() as Promise<Country[]>;
+export default async function getCountry(): Promise<Country[]> {
+  const result: Response = await fetch(url);
+  const data = await result.json() as Country[];
+  sessionStorage.setItem("countries", JSON.stringify(data));
+
+  return data;
 }
