cb080585580e0de7b7b1026518ad3e5d6ac68cee
Rem0ld <p.lovergne@hotmail.fr>
2021-05-21 10:27:53 +0200

    added new fns for more control of the session storage

--- src/helpers/sessionStorage.ts
+++ src/helpers/sessionStorage.ts
@@ -1,15 +1,34 @@
-import { Country } from "types";
+import { Continent, Country, Hobby } from "types";
 
 
-export function setSessionStorage(
-  authorised: Country[],
-  banned: Country[]
+export function setSessionStorageIncludedExcluded(
+  included: Country[],
+  excluded: Country[]
 ): void {
-  sessionStorage.setItem("authorised", JSON.stringify(authorised));
-  sessionStorage.setItem("banned", JSON.stringify(banned));
+  sessionStorage.setItem("included", JSON.stringify(included));
+  sessionStorage.setItem("excluded", JSON.stringify(excluded));
 };
 
-export function getSessionStorage(): Country[] | undefined {
-  const list: string | null = sessionStorage.getItem("authorised")
-  return list ? JSON.parse(list) as Country[] : undefined;
+export function getSessionStorageIncluded(): Country[] {
+  const list: string = sessionStorage.getItem("included") as string
+  return JSON.parse(list) as Country[];
+}
+
+export function getSessionStorageExcluded(): Country[] {
+  const list: string = sessionStorage.getItem("excluded") as string;
+  return JSON.parse(list) as Country[];
+}
+
+export function getSessionStorage(argument: string): Country[] {
+  const list: string = sessionStorage.getItem(argument) as string;
+  return JSON.parse(list) as Country[];
+}
+
+export function setSessionStorageFilter(name: string, element: Continent | Hobby): void {
+  sessionStorage.setItem(name, JSON.stringify(element));
+}
+
+export function getSessionStorageFilter(name: string): Continent | Hobby {
+  const filter: string = sessionStorage.getItem(name) as string;
+  return JSON.parse(filter) as Continent | Hobby;
 }
