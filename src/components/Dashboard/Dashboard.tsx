9c5de1f355ce33f6441a8663d757f3d9d71eb667
Rem0ld <p.lovergne@hotmail.fr>
2021-05-21 10:29:40 +0200

    new fns, btn reset all, working state

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,27 +1,30 @@
+/* eslint-disable unicorn/no-array-reduce */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-restricted-syntax */
-import magnifyIcon from "@iconify-icons/mdi-light/magnify";
-import { InlineIcon } from "@iconify/react";
+import FilterInput from "components/FilterInput/FilterInput";
 import Filters from "components/Filters/Filters";
 import { motion, useCycle } from "framer-motion";
 import React, { ReactElement, useCallback, useEffect, useState } from "react";
-import { Continents, Country, Hobbies } from "types";
+import { Continent, Country, Filter, Hobby } from "types";
 import listDangerousCountry from "../../api/listDangerousCountry";
-import { setSessionStorage } from "../../helpers/sessionStorage";
+import {
+  getSessionStorage,
+  getSessionStorageExcluded,
+  getSessionStorageFilter,
+  getSessionStorageIncluded,
+  setSessionStorageFilter,
+  setSessionStorageIncludedExcluded,
+} from "../../helpers/sessionStorage";
 import ButtonToggle from "../elements/ButtonToggle";
 import DashboardList from "./DashboardList/DashboardList";
 import classes from "./styles";
 
 // To move to external file
-const hobbies: Hobbies = { beach: true, mountain: true };
+const hobbies: string[] = ["beach", "mountain"];
 
 interface AppProperties {
   countries: Country[];
 }
-
-const toggleFilter = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-  console.log(event);
-};
 
 // Objects used by framer-motion for animations
 const variants = {
@@ -36,58 +39,128 @@ export default function Dashboard({ countries }: AppProperties): ReactElement {
   const [isOpen, toggleOpen] = useCycle(false, true);
   const [includedCountry, setIncludedCountry] = useState<Country[]>([]);
   const [excludedCountry, setExcludedCountry] = useState<Country[]>([]);
-  const [continents, setContinents] = useState<Continents>();
+  const [continents, setContinents] = useState<Continent>({});
 
   /**
-   * Sorts countries by continents
-   *
-   * { continentName: Countries[] }
+   * Will create filters with specific object pattern
+   * subject to change in future
    */
-  const makeContinents = useCallback(
-    (): Continents =>
-      // eslint-disable-next-line unicorn/no-array-reduce
-      countries.reduce(
-        (accumulator: Continents, element: Country): Continents => {
-          const cle = element.region;
-          if (!accumulator[cle]) {
-            accumulator[cle] = [];
-          }
-          accumulator[cle].push(element);
-          return accumulator;
-        },
-        {}
-      ),
-    [countries]
-  );
+  const makeFilters = useCallback((): Filter => {
+    console.log("I'm changing");
+
+    const continentsFiltered = countries.reduce(
+      (accumulator: Continent, country: Country): Continent => {
+        const key = country.region;
+        if (!accumulator[key]) {
+          accumulator[key] = {
+            list: [],
+            isChecked: false,
+          };
+        }
+        accumulator[key].list.push(country);
+        return accumulator;
+      },
+      {}
+    );
+
+    const hobbiesFiltered = hobbies.reduce(
+      (accumulator: Hobby, hobby: string): Hobby => {
+        const key = hobby;
+        if (!accumulator[key]) {
+          accumulator[key] = {
+            list: undefined,
+            isChecked: false,
+          };
+        }
+        return accumulator;
+      },
+      {}
+    );
+    setSessionStorageFilter("continents", continentsFiltered);
+    setSessionStorageFilter("hobbies", hobbiesFiltered);
+
+    setContinents(continentsFiltered);
+    return { continents: continentsFiltered, hobbies: hobbiesFiltered };
+  }, [countries]);
+
+  /**
+   * Will update lists when user is (un)checking box from filters
+   */
+  const updateListsPerContinents = () => {
+    interface List {
+      list: Country[];
+      isChecked: boolean;
+    }
+
+    const list: Continent | Hobby = getSessionStorageFilter("continents");
+    let included: Country[] = [];
+    let excluded: Country[] = [];
+
+    for (const [_, object] of Object.entries(list)) {
+      const element: List = object as List;
+
+      if (element.isChecked) {
+        included = [...included, ...element.list];
+      } else {
+        excluded = [...excluded, ...element.list];
+      }
+    }
+    setSessionStorageIncludedExcluded(included, excluded);
+    setIncludedCountry(included);
+    setExcludedCountry(excluded);
+  };
+
+  /**
+   * Will modify isChecked continent state
+   * @param element Name of continent that needs to update
+   */
+  const toggleCheckboxContinents = (element: string) => {
+    const newState: Continent = {
+      ...continents,
+      [element]: {
+        ...continents[element],
+        isChecked: !continents[element].isChecked,
+      },
+    };
+    setContinents(newState);
+    setSessionStorageFilter("continents", newState);
+    updateListsPerContinents();
+  };
 
   /**
    * Filters countries in 2 lists: included, excluded ( from the pool )
    * Will fire when we receive the list of countries
-   * Subject to change
+   * Function subject to change in the future
    */
   const firstFiltering = useCallback(
     (list: string[]) => {
-      const included: Country[] = [];
-      const excluded = countries.filter((element) => {
-        for (const country of list) {
-          if (element.name.includes(country)) {
-            return 1;
+      let included: Country[] = [];
+      let excluded: Country[] = [];
+
+      // If already in sessionStorage we use those values
+      if (
+        getSessionStorageIncluded() &&
+        getSessionStorageIncluded().length > 0
+      ) {
+        included = getSessionStorageIncluded();
+        excluded = getSessionStorageExcluded();
+      } else {
+        excluded = countries.filter((element) => {
+          for (const country of list) {
+            if (element.name.includes(country)) {
+              return 1;
+            }
           }
-        }
-        included.push(element);
-        return 0;
-      });
+          included.push(element);
+          return 0;
+        });
+        setSessionStorageIncludedExcluded(included, excluded);
+      }
       setIncludedCountry(included);
       setExcludedCountry(excluded);
-      setSessionStorage(included, excluded);
     },
     [countries]
   );
-
-  useEffect(() => {
-    setContinents(makeContinents());
-    firstFiltering(listDangerousCountry);
-  }, [countries, firstFiltering, makeContinents]);
 
   /**
    * Will remove the country from included and put it in excluded
@@ -106,7 +179,7 @@ export default function Dashboard({ countries }: AppProperties): ReactElement {
       (previousState): Country[] => [...previousState, country] as Country[]
     );
 
-    setSessionStorage(filteredList, excludedCountry);
+    setSessionStorageIncludedExcluded(filteredList, excludedCountry);
   };
 
   /**
@@ -125,8 +198,55 @@ export default function Dashboard({ countries }: AppProperties): ReactElement {
     setIncludedCountry(
       (previousState): Country[] => [...previousState, country] as Country[]
     );
-    setSessionStorage(includedCountry, filteredList);
+    setSessionStorageIncludedExcluded(includedCountry, filteredList);
   };
+
+  /**
+   * Removes all country from a list and add it to the other one
+   * @param argument included / excluded
+   */
+  const removeAll = (argument: string): void => {
+    let included: Country[] = [];
+    let excluded: Country[] = [];
+
+    if (argument === "included") {
+      excluded = getSessionStorage("countries");
+    } else {
+      included = getSessionStorage("countries");
+    }
+
+    setIncludedCountry(included);
+    setExcludedCountry(excluded);
+    setSessionStorageIncludedExcluded(included, excluded);
+  };
+
+  /**
+   * Will reset all kind of filters
+   * @param list list of dangerous country to remove in any case
+   */
+  const reset = (list: string[]): void => {
+    const included: Country[] = [];
+    let excluded: Country[] = [];
+
+    excluded = countries.filter((element) => {
+      for (const country of list) {
+        if (element.name.includes(country)) {
+          return 1;
+        }
+      }
+      included.push(element);
+      return 0;
+    });
+    setSessionStorageIncludedExcluded(included, excluded);
+    setIncludedCountry(included);
+    setExcludedCountry(excluded);
+    makeFilters();
+  };
+
+  useEffect(() => {
+    firstFiltering(listDangerousCountry);
+    makeFilters();
+  }, [countries, firstFiltering, makeFilters]);
 
   return (
     <motion.div
@@ -136,38 +256,40 @@ export default function Dashboard({ countries }: AppProperties): ReactElement {
       initial={{ x: "-100%" }}
       className={classes.component}
     >
+      {/* Button to open Dashboard */}
       <ButtonToggle
         toggle={() => {
           toggleOpen();
         }}
       />
       <h2 className="text-lg font-bold text-secondary">Options:</h2>
-      <div className="py-4">
-        <p className="text-sm pl-2">Country to remove</p>
-        <div className="flex items-center -pt-2">
-          <InlineIcon icon={magnifyIcon} />
-          <input
-            type="text"
-            aria-label="Full name"
-            tabIndex={-1}
-            className={classes.input}
-          />
-        </div>
-      </div>
-      <Filters
-        listFilters={[continents as Continents, hobbies]}
-        toggleFilter={toggleFilter}
+
+      {/* Input component */}
+      <FilterInput
+        includedCountry={includedCountry}
+        banACountry={banACountry}
       />
+
+      {/* Filters */}
+      <Filters
+        continents={continents}
+        toggleCheckboxContinents={toggleCheckboxContinents}
+        resetAll={reset}
+      />
+
+      {/* List included, excluded */}
       <div className="flex sm:flex-row flex-col justify-between">
         <DashboardList
           title="Excluded"
           list={excludedCountry}
           onclick={authoriseACountry}
+          removeAll={removeAll}
         />
         <DashboardList
           title="Included"
           list={includedCountry}
           onclick={banACountry}
+          removeAll={removeAll}
         />
       </div>
     </motion.div>
--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -1,27 +1,5 @@
 import ctl from "helpers/ctl";
 
-const input = ctl(`
-appearance-none 
-bg-transparent 
-border-t-0 
-border-r-0 
-border-l-0 
-border-b-1 
-border-gray-300 
-w-2/4 
-mr-3 
-py-1 
-px-2 
-leading-tight
-focus:outline-none 
-focus:ring-white 
-focus:bg-white
-focus:border-primary
-focus:border-b-2
-hover:border-primary
-hover:border-b-2
-`);
-
 const component = ctl(`
 absolute
 left-0
@@ -38,6 +16,5 @@ bg-white
 `)
 
 export default {
-  input,
   component
 }
