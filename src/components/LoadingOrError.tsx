0aa91939bbb6ee796c7998295228fecd5976073c
Rem0ld <p.lovergne@hotmail.fr>
2021-05-22 21:36:23 +0200

    up to date and working

--- src/components/LoadingOrError.tsx
+++ src/components/LoadingOrError.tsx
@@ -1,6 +1,8 @@
+import { motion } from "framer-motion";
 import Lottie from "lottie-react";
 import React, { ReactElement } from "react";
 import loader from "../../public/loading_earth_globe.json";
+import train from "../../public/train.json";
 
 interface Properties {
   error?: Error;
@@ -12,10 +14,28 @@ export default function LoadingOrError({ error }: Properties): ReactElement {
         {error ? (
           error.message
         ) : (
-          <Lottie
-            animationData={loader}
-            className="md:w-96 md:h-96 w-60 h-60"
-          />
+          <div>
+            <Lottie
+              animationData={loader}
+              className="md:w-96 md:h-96 w-60 h-60"
+            />
+            <motion.div
+              className="absolute z-0 transform-gpu bottom-10 right-full -translate-y-full -translate-x-full"
+              animate={{
+                x: 2000,
+              }}
+              transition={{
+                duration: 5,
+                ease: "easeInOut",
+                repeat: Number.POSITIVE_INFINITY,
+              }}
+            >
+              <Lottie
+                animationData={train}
+                className="scale-spe md:w-30 md:h-30 w-20 h-20"
+              />
+            </motion.div>
+          </div>
         )}
       </h1>
     </div>
