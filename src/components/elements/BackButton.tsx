2bc1eb24149c503cd4660b8dca34dab844ac6a14
Rem0ld <p.lovergne@hotmail.fr>
2021-05-22 11:48:33 +0200

    made new btns and icon backButton validIcon

---
+++ src/components/elements/BackButton.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import BackIcon from "example-components/BackIcon";
+import React, { ReactElement } from "react";
+import { useHistory } from "react-router-dom";
+
+export default function BackButton(): ReactElement {
+  const history = useHistory();
+  return (
+    <div
+      data-cy="BackLink"
+      className="flex items-center gap-2 text-primary hover:text-secondary font-bold"
+      onClick={() => history.goBack()}
+      role="button"
+      tabIndex={0}
+    >
+      <BackIcon />
+      Back
+    </div>
+  );
+}
