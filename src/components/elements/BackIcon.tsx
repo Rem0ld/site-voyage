13f2cb7109c0819eb53471cbf816d13c77b177d0
Rem0ld <p.lovergne@hotmail.fr>
2021-05-22 08:48:31 +0200

    added new icons + component backIcon

---
+++ src/components/elements/BackIcon.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from "react";
+
+export default function BackIcon(): ReactElement {
+  return (
+    <svg
+      width="18"
+      height="14"
+      viewBox="0 0 18 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 1L1 7L8 13"
+        stroke="#84CC16"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1 7H17"
+        stroke="#84CC16"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
