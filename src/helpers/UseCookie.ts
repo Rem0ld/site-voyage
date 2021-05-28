ff7ec029ceada6bff5c087fba7ca4383414a12b6
Rem0ld <p.lovergne@hotmail.fr>
2021-05-28 15:24:08 +0200

    hook, not in use atm

---
+++ src/helpers/UseCookie.ts
@@ -0,0 +1,19 @@
+import Cookies from "js-cookie";
+import { useEffect } from "react";
+import { User } from "types";
+import auth from "../../firebase-auth";
+
+export default function useCookie() {
+  // const [isCookie, setIsCookie] = useState(false);
+
+  useEffect(() => {
+    const user = Cookies.getJSON("user") as User;
+    console.log("in useCookie", user)
+    if (!user || user === null || user === undefined) {
+      console.log("condition is true")
+      auth.signOut().finally(() => { })
+      localStorage.removeItem("@token")
+      Cookies.remove("user")
+    }
+  }, [])
+}
