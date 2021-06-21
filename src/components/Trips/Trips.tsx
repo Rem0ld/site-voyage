5ff2f45b9feaf2cd1896554dcca3b9cf0cdab121
Rem0ld <p.lovergne@hotmail.fr>
2021-06-21 15:48:32 +0200

    changed import

--- src/components/Trips/Trips.tsx
+++ src/components/Trips/Trips.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import deleteNotification from "api/server/notificationRoute";
+import { deleteNotification } from "api/server/notificationRoute";
 import { deleteTravel, getTravel, updateTravel } from "api/server/TravelRoutes";
 import React, { ReactElement, useEffect, useState } from "react";
 import { Travel } from "types";
